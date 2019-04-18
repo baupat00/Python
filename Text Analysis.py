5f714b24f589d17b53a94d875e20c7685f89da7e
baupat00 <baumann.p@protonmail.ch>
2019-04-18 16:50:42 +0200

    My first Push

---
+++ Text Analysis.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 16:50:12 2019
+
+@author: Patrick
+"""
+# import package and its set of stopwords
+from wordcloud import WordCloud, STOPWORDS
+
+print ('Wordcloud is installed and imported!')
+#%%
